9c3db9ed86399fdd6bba2288eb1aa5aa0205acc0
Mowd <mowd8574@gmail.com>
2020-03-15 21:50:52 +0800

    Add search function and make filter work.

--- js/main.js
+++ js/main.js
@@ -28,10 +28,70 @@ function getWeekDate(dt) {
 
 var se = {};
 
+se.Search = {
+    setCallback: function() {
+        $(".btn-search").on("click", function() {
+            var keyword = $(".search-keyword").val();
+            se.Search.search(keyword);
+        });
+    },
+    search: function(keyword) {
+        var professor_list = se.Filter.Professor.getSelected();
+        var conference_list = se.Filter.Conference.getSelected();
+        if(professor_list.length > 0) {
+            for(var i in professor_list) {
+                keyword += " author:" + professor_list[i].replace(/-/g, ' ') + ": ";
+            }
+        }
+        if(conference_list.length > 0) {
+            keyword += " venue:" + conference_list[0] + ": ";
+        }
+        $.getJSON("http://dblp.org/search/publ/api?format=jsonp&q=" + keyword + "&callback=?", function(res) {
+            var output = [];
+            res = res.result.hits.hit;
+            for(var i in res) {
+                var author_li = [];
+                console.log(res[i]);
+                for(var j in res[i]["info"]["authors"]["author"]) {
+                    author_li.push(res[i]["info"]["authors"]["author"][j]["text"]);
+                }
+                output.push(
+                    res[i]["info"]["year"] + "<br />",
+                    author_li.join(", ") + "<br />",
+                    res[i]["info"]["title"] + " " + res[i]["info"]["venue"] + " " + res[i]["info"]["pages"],
+                    '<a href="' + res[i]["info"]["url"] + '" target="_blank">[VIEW]</a>',
+                    "<hr>"
+                );
+            }
+            $(".search-result").html(output.join("\n"));
+        });
+    },
+};
+
 se.Filter = {
     setCallback: function() {
         se.Filter.Conference.render();
         se.Filter.Professor.render();
+        se.Filter.searchCallback();
+    },
+    searchCallback: function() {
+        $(".filter-search").on("keyup", function(e) {
+            var target = $(this).attr("target");
+            var keyword = $(this).val().toLowerCase();
+            if($(this).val() == "") {
+                $(target).find("label").show();
+            }
+            else {
+                $(target).find("label").each(function() {
+                    if($(this).attr("text").indexOf(keyword) == -1) {
+                        $(this).hide();
+                    }
+                    else {
+                        $(this).show();
+                    }
+                });
+            }
+        });
     },
     Conference: {
         render: function() {
@@ -46,7 +106,7 @@ se.Filter = {
                             short = full.substr(0, 7) + "...";
                         }
                         li.push(
-                            '<input type="checkbox" /> <span title="' + full +' ">' + short + '</span>'
+                            '<div><label text="' + short.toLowerCase() + '"><input type="checkbox" class="chkbox-conference" value="' + short + '" /> <span title="' + full +' ">' + short + '</span></label></div>'
                         );
                     }
                     var panel = [
@@ -54,13 +114,22 @@ se.Filter = {
                         '    <div class="panel-heading">',
                         '        <h3 class="panel-title">' + i + '</h3>',
                         '    </div>',
-                        '    <div class="panel-body">' + li.join("<br>") + '</div>',
+                        '    <div class="panel-body">' + li.join("\n") + '</div>',
                         '</div>'
                     ];
                     html.push(panel.join("\n"));
                 }
                 $(".conference-block").html(html.join("\n"));
             });
+        },
+        getSelected: function() {
+            var li = [];
+            $(".chkbox-conference").each(function() {
+                if($(this).prop("checked")) {
+                    li.push($(this).val());
+                }
+            });
+            return li;
         }
     },
     Professor: {
@@ -70,7 +139,7 @@ se.Filter = {
                 var li = [];
                 for(var i in data) {
                     li.push(
-                        '<input type="checkbox" /> ' + data[i]
+                        '<div><label text="' + data[i].toLowerCase() + '"><input type="checkbox" class="chkbox-professor" value="' + data[i] + '" /> ' + data[i] + '</label></div>'
                     );
                 }
                 var panel = [
@@ -78,11 +147,20 @@ se.Filter = {
                     '    <div class="panel-heading">',
                     '        <h3 class="panel-title">NCTU</h3>',
                     '    </div>',
-                    '    <div class="panel-body">' + li.join("<br>") + '</div>',
+                    '    <div class="panel-body">' + li.join("\n") + '</div>',
                     '</div>'
                 ];
                 $(".professor-block").html(panel.join("\n"));
             });
+        },
+        getSelected: function() {
+            var li = [];
+            $(".chkbox-professor").each(function() {
+                if($(this).prop("checked")) {
+                    li.push($(this).val());
+                }
+            });
+            return li;
         }
     }
 }
